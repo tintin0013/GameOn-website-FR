b4dcf7619485f1ce3eeacd0222d09010a72286af
tintin0013 <tintin0013@gmail.com>
2022-05-03 11:15:33 +0200

    mise en place des fonctions de controles du formulaire

--- js/modal.js
+++ js/modal.js
@@ -13,7 +13,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-const closeBtn = document.getElementsByClassName('close');
+const closeBtn = document.querySelector('.close');
+
 
 
 // launch modal event
@@ -22,13 +23,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  const first = document.getElementById('first');
+  first.focus();
 }
 
 // close modal 
 
-function closeModal() {
-  modalbg.style.display = 'none';
-}
-closeBtn[0].addEventListener('click', closeModal);
+closeBtn.addEventListener('click', () => {
+  modalbg.style.display = "none";
+  form.reset();
+});
 
 
